a3c0f0ece9555a1ba406e3aa7811508cefac69ee
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-01-31 16:48:16 -0300

    adicionando arquivo de rotas da api

---
+++ src/routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import usuarioController from "./controllers/UsuarioController";
+import refeicaoController from "./controllers/RefeicaoController";
+
+const routes = Router();
+
+routes.get('/usuario', usuarioController.index);
+routes.get('/usuario/:id', usuarioController.show);
+routes.post('/usuario', usuarioController.create);
+routes.put('/usuario/:id', usuarioController.update);
+routes.delete('/usuario/:id', usuarioController.delete);
+
+routes.get('/refeicao', refeicaoController.index);
+routes.get('/refeicao/:id', refeicaoController.show);
+routes.post('/refeicao', refeicaoController.create);
+routes.put('/refeicao/:id', refeicaoController.update);
+routes.delete('/refeicao/:id', refeicaoController.delete);
+
+export default routes;
