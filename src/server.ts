bc9a8789de0e30d75f932cbe049c2d18d8eaa094
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-14 22:26:47 -0300

    alterando arquivo

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,10 @@ import routes from "./routes";
 import errorHandler from "./errors/handler";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
+
 app.listen(3333, () => console.log("Express iniciado..."));
