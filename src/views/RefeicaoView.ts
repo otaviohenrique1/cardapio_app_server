fe8a3415df0640649e6f66c7cf6a30e0e8d704a7
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-23 19:46:40 -0300

    adicionando logica do upload da imagem

--- src/views/RefeicaoView.ts
+++ src/views/RefeicaoView.ts
@@ -1,4 +1,5 @@
 import Refeicao from "../entity/Refeicao";
+import ImagemView from "./ImagemView";
 
 export default {
   render(refeicao: Refeicao) {
@@ -8,8 +9,14 @@ export default {
       preco: refeicao.preco,
       ingredientes: refeicao.ingredientes,
       ativo: refeicao.ativo,
+      codigo: refeicao.codigo,
       data_cadastro: refeicao.data_cadastro,
+      data_modificacao_cadastro: refeicao.data_modificacao_cadastro,
       id_usuario: refeicao.id_usuario,
+      images: ImagemView.renderMany(refeicao.imagens),
     };
+  },
+  renderMany(refeicoes: Refeicao[]) {
+    return refeicoes.map(refeicao => this.render(refeicao));
   }
 };
