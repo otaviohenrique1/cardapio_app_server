3d7af8e7dfe2577d05e4151004be7a523c83179b
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-30 21:25:53 -0300

    alterando url da imagem

--- src/views/ImagemView.ts
+++ src/views/ImagemView.ts
@@ -2,9 +2,12 @@ import { Imagem } from "../entity/Imagem";
 
 export default {
   render(imagem: Imagem) {
+    const usuario_codigo = imagem.refeicao.usuario.codigo;
+    const usuario_id = imagem.refeicao.usuario.id;
+    const imagem_path = imagem.path;
     return {
       id: imagem.id,
-      url: `http://192.168.0.12:3333/uploads/fotos/${imagem.path}`,
+      url: `http://192.168.0.12:3333/uploads/fotos/${usuario_id}_${usuario_codigo}/${imagem_path}`,
     };
     // url: `http://localhost:3333/uploads/${image.path}`,
   },
