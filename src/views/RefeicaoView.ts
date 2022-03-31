b8c7e7471bc607c400f6e613801865d0b1e8f812
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-30 21:27:05 -0300

    alterando id_usuario e imagens

--- src/views/RefeicaoView.ts
+++ src/views/RefeicaoView.ts
@@ -13,8 +13,8 @@ export default {
       codigo: refeicao.codigo,
       data_cadastro: refeicao.data_cadastro,
       data_modificacao_cadastro: refeicao.data_modificacao_cadastro,
-      id_usuario: refeicao.id_usuario,
-      images: ImagemView.renderMany(refeicao.imagens),
+      id_usuario: refeicao.usuario.id,
+      imagens: ImagemView.renderMany(refeicao.imagens),
     };
   },
   renderMany(refeicoes: Refeicao[]) {
