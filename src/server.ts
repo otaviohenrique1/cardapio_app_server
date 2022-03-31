ca6fc9a64763cd91fd0a99fd5df4aacafb38ead8
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-30 21:24:40 -0300

    removendo codigo comentado

--- src/server.ts
+++ src/server.ts
@@ -5,20 +5,13 @@ import 'express-async-errors';
 import "./connection";
 import routes from "./routes";
 import errorHandler from "./errors/handler";
-import cookieParser from "cookie-parser";
-// import createError from "http-errors";
-// import morgan from "morgan";
-// import multer from "multer";
 import { PORT } from "./config/constantes";
-// import { MULTER_OPTIONS_DEST } from "./config/constantes";
-// const upload = multer({ dest: MULTER_OPTIONS_DEST })
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cookieParser());
 app.use('/uploads/fotos', express.static(path.join(__dirname, '..', 'uploads', 'fotos')));
 app.use(errorHandler);
 
