9dcf6dc005ebfb5556d8415f66ccd4e7d5ed1175
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-31 22:46:37 -0300

    adicionando function slug

--- src/config/utils.ts
+++ src/config/utils.ts
@@ -1,3 +1,26 @@
 export function MensagemCampoVazio(campo_nome: string): string {
   return `Campo ${campo_nome} vazio`;
 }
+
+export function slug(valor: any): string {
+  return String(valor)
+    .toString()
+    .toLowerCase()
+    .replace(/[àÀáÁâÂãäÄÅåª]+/g, 'a')
+    .replace(/[èÈéÉêÊëË]+/g, 'e')
+    .replace(/[ìÌíÍîÎïÏ]+/g, 'i')
+    .replace(/[òÒóÓôÔõÕöÖº]+/g, 'o')
+    .replace(/[ùÙúÚûÛüÜ]+/g, 'u')
+    .replace(/[ýÝÿŸ]+/g, 'y')
+    .replace(/[ñÑ]+/g, 'n')
+    .replace(/[çÇ]+/g, 'c')
+    .replace(/[ß]+/g, 'ss')
+    .replace(/[Ææ]+/g, 'ae')
+    .replace(/[Øøœ]+/g, 'oe')
+    .replace(/[%]+/g, 'pct')
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+}
