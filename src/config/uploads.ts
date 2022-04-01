bd782fc8f9db1f7346dff910ce87ced101132e4e
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-31 22:51:17 -0300

    adicionando slug, alterando destination e filename

--- src/config/uploads.ts
+++ src/config/uploads.ts
@@ -1,22 +1,27 @@
-import { request } from "express";
+// import { request } from "express";
 import multer from "multer";
 import path from "path";
 import crypto from 'crypto';
 import fs from "fs";
+// import { slug } from "./utils";
 
+/* arrumar parte do nome do arquivo */
 export default {
   storage: multer.diskStorage({
     destination: function (request, file, cb) {
       const { id, codigo } = request.body
-      const fileDestination2 = path.join(__dirname, '..', '..', 'uploads', 'fotos', `${id}_${codigo}`);
+      const fileDestination = path.join(__dirname, '..', '..', 'uploads', 'fotos', `${codigo}_${id}`);
+      fs.mkdirSync(fileDestination, { recursive: true })
       // const fileDestination = path.join(__dirname, '..', '..', 'uploads', 'fotos');
-      cb(null, fileDestination2);
+      cb(null, fileDestination);
     },
     filename: (request, file, cb) => {
-      // const { id, codigo } = request.body
+      const { id, codigo } = request.body
 
-      // const fileName = `${id}-${codigo}-${crypto.randomBytes(3).toString('hex')}-${Date.now()}-${file.originalname}`;
-      const fileName = `${crypto.randomBytes(3).toString('hex')}-${Date.now()}-${file.originalname}`;
+      const nomeDoArquivo = file.originalname;
+      const fileName = `${id}-${codigo}-${crypto.randomBytes(3).toString('hex')}-${Date.now()}-${nomeDoArquivo}`;
+      // const fileName = `${id}-${codigo}-${crypto.randomBytes(3).toString('hex')}-${Date.now()}-${slug(nomeDoArquivo)}`;
+      // const fileName = `${crypto.randomBytes(3).toString('hex')}-${Date.now()}-${file.originalname}`;
       cb(null, fileName);
     }
   })
