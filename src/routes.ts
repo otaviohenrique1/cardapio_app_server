cadd419742f40f5d242351ed1fe348d6e35d0b5f
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-31 22:46:09 -0300

    adicionando query_param codigo

--- src/routes.ts
+++ src/routes.ts
@@ -8,16 +8,16 @@ const routes = Router();
 const upload = multer(uploadConfig);
 
 routes.get('/usuario', usuarioController.index);
-routes.get('/usuario/:id', usuarioController.show);
+routes.get('/usuario/:codigo', usuarioController.show);
 routes.post('/usuario', usuarioController.create);
-routes.put('/usuario/:id', usuarioController.update);
-routes.delete('/usuario/:id', usuarioController.delete);
+routes.put('/usuario/:codigo', usuarioController.update);
+routes.delete('/usuario/:codigo', usuarioController.delete);
 routes.post('/usuario/login', usuarioController.login);
 
-routes.get('/refeicao', refeicaoController.index);
-routes.get('/refeicao/:id', refeicaoController.show);
+routes.get('/refeicao/:codigo', refeicaoController.index);
+routes.get('/refeicao/:codigo', refeicaoController.show);
 routes.post('/refeicao', upload.array('imagem'), refeicaoController.create);
-routes.put('/refeicao/:id', upload.array('imagem'), refeicaoController.update);
-routes.delete('/refeicao/:id', refeicaoController.delete);
+routes.put('/refeicao/:codigo', upload.array('imagem'), refeicaoController.update);
+routes.delete('/refeicao/:codigo', refeicaoController.delete);
 
 export default routes;
