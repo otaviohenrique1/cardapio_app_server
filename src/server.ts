92080def60652772606499e001fe12c66147c1ca
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-31 22:51:47 -0300

    alterando connection e comentando morgan

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,16 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import 'express-async-errors';
-import "./connection";
+import "./config/connection";
 import routes from "./routes";
 import errorHandler from "./errors/handler";
 import { PORT } from "./config/constantes";
+// import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
+// app.use(morgan('combined'));
 app.use(express.json());
 app.use(routes);
 app.use('/uploads/fotos', express.static(path.join(__dirname, '..', 'uploads', 'fotos')));
