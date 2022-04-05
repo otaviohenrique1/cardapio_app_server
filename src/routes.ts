d41eef2ebefc501f4feb17b69de5ed0fbd0a2e7f
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-04 22:58:06 -0300

    alterando request.body

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import usuarioController from "./controllers/UsuarioController";
 import refeicaoController from "./controllers/RefeicaoController";
+import pedidoController from "./controllers/PedidoController";
 import multer from "multer";
 import uploadConfig from "./config/uploads";
 
@@ -19,5 +20,11 @@ routes.get('/refeicao/:codigo', refeicaoController.show);
 routes.post('/refeicao', upload.array('imagem'), refeicaoController.create);
 routes.put('/refeicao/:codigo', upload.array('imagem'), refeicaoController.update);
 routes.delete('/refeicao/:codigo', refeicaoController.delete);
+
+routes.get('/pedido/:codigo', pedidoController.index);
+routes.get('/pedido/:codigo', pedidoController.show);
+routes.post('/pedido', pedidoController.create);
+routes.put('/pedido/:codigo', pedidoController.update);
+// routes.delete('/pedido/:codigo', pedidoController.delete);
 
 export default routes;
