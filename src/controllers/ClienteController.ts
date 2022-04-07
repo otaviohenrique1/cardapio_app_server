9832f2eaa9006ce56efdf55a2e04aa2377cd3cc0
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-07 19:14:37 -0300

    adicionando login2

--- src/controllers/ClienteController.ts
+++ src/controllers/ClienteController.ts
@@ -1,11 +1,65 @@
 import { NextFunction, Request, Response } from "express";
 import { getRepository } from "typeorm";
 import * as Yup from "yup";
+import { EMAIL_INVALIDO, LOGADO_COM_SUCESSO, LOGIN_INVALIDO, SENHA_INVALIDO, USUARIO_INVALIDO } from "../config/constantes";
 import { MensagemCampoVazio } from "../config/utils";
 import { Cliente } from "../entity/Cliente";
 import clienteView from "../views/ClienteView";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import { compareSync } from "bcrypt";
 
 export default {
+  // async login2(request: Request, response: Response, next: NextFunction) {
+  //   const { email, senha } = request.body;
+    
+  //   passport.use(new Strategy({
+  //     usernameField: email,
+  //     passwordField: senha
+  //   }, async function verify(email, senha, done) {
+  //     const clienteRepository = getRepository(Cliente);
+  //     const cliente = await clienteRepository.findOne({ email: email });
+  //     try {
+  //       if (!cliente) return done(null, false, { message: USUARIO_INVALIDO});
+        
+  //       const isValid = compareSync(senha, cliente.senha);
+
+  //       if (!isValid) return done(null, false, { message: SENHA_INVALIDO});
+
+  //       return done(null, cliente);
+  //     } catch (error) {
+  //       done(error, false)
+  //     }
+  //   }));
+  // },
+  async login(request: Request, response: Response, next: NextFunction) {
+    const { email, senha } = request.body;
+    // console.log(email);
+    // console.log(senha);
+
+    let existingUser: any;
+    const clienteRepository = getRepository(Cliente);
+    try {
+      existingUser = await clienteRepository.findOne({ email: email });
+    } catch (error) {
+      return response.status(500).json({ message: LOGIN_INVALIDO });
+    }
+
+    if (!existingUser) {
+      return response.status(401).json({ message: USUARIO_INVALIDO });
+    } else if (existingUser.senha !== senha) {
+      return response.status(401).json({ message: SENHA_INVALIDO });
+    } else if (existingUser.email !== email) {
+      return response.status(401).json({ message: EMAIL_INVALIDO });
+    }
+    let data_user = {
+      id: existingUser.id,
+      nome: existingUser.nome,
+      email: existingUser.email,
+      codigo: existingUser.codigo,
+    };
+    return response.status(200).json({ message: LOGADO_COM_SUCESSO, data_user });
+  },
   /**
    * Listar todas os pedidos cadastrados
    */
@@ -15,12 +69,12 @@ export default {
     return response.json(cliente);
   },
   /**
-   * Busca um pedido cadastrado usando o codigo do mesmo e exibe os seus dados
+   * Busca um pedido cadastrado usando o id do mesmo e exibe os seus dados
    */
   async show(request: Request, response: Response, next: NextFunction) {
-    const { codigo } = request.params;
+    const { id } = request.params;
     const clienteRepository = getRepository(Cliente);
-    const cliente = await clienteRepository.findOneOrFail(codigo);
+    const cliente = await clienteRepository.findOneOrFail(id);
     return response.json(clienteView.render(cliente));
   },
   /**
@@ -38,7 +92,7 @@ export default {
       cidade: Yup.string().required(MensagemCampoVazio('cidade')),
       estado: Yup.string().required(MensagemCampoVazio('estado')),
       cep: Yup.string().required(MensagemCampoVazio('cep')),
-      telefone1: Yup.string().required(MensagemCampoVazio('telefone')),
+      telefone: Yup.string().required(MensagemCampoVazio('telefone')),
       email: Yup.string().required(MensagemCampoVazio('email')),
       senha: Yup.string().required(MensagemCampoVazio('senha')),
       data_cadastro: Yup.date().required(MensagemCampoVazio('data_cadastro')),
@@ -50,19 +104,19 @@ export default {
     return response.status(201).json(cliente);
   },
   /**
-   * Apaga um pedido, usando o codigo do mesmo
+   * Apaga um pedido, usando o id do mesmo
    */
   // async delete(request: Request, response: Response, next: NextFunction) {
-  //   const { codigo } = request.params;
+  //   const { id } = request.params;
   //   const clienteRepository = getRepository(Cliente);
-  //   const cliente = await clienteRepository.delete(codigo);
+  //   const cliente = await clienteRepository.delete(id);
   //   return response.status(200).json(cliente);
   // },
   /**
-   * Atualiza os dados de um pedido, usando o codigo do mesmo para busca-lo no banco de dados
+   * Atualiza os dados de um pedido, usando o id do mesmo para busca-lo no banco de dados
    */
   async update(request: Request, response: Response, next: NextFunction) {
-    const { codigo, nome, data_modificacao_cadastro, rua, numero, bairro, cidade, estado, cep, telefone, email, senha } = request.body;
+    const { id, nome, data_modificacao_cadastro, rua, numero, bairro, cidade, estado, cep, telefone, email, senha } = request.body;
     const clienteRepository = getRepository(Cliente);
     const data = { nome, data_modificacao_cadastro, rua, numero, bairro, cidade, estado, cep, telefone, email, senha };
     const schema = Yup.object().shape({
@@ -73,13 +127,13 @@ export default {
       cidade: Yup.string().required(MensagemCampoVazio('cidade')),
       estado: Yup.string().required(MensagemCampoVazio('estado')),
       cep: Yup.string().required(MensagemCampoVazio('cep')),
-      telefone1: Yup.string().required(MensagemCampoVazio('telefone')),
+      telefone: Yup.string().required(MensagemCampoVazio('telefone')),
       email: Yup.string().required(MensagemCampoVazio('email')),
       senha: Yup.string().required(MensagemCampoVazio('senha')),
       data_modificacao_cadastro: Yup.date().required(MensagemCampoVazio('data_modificacao_cadastro')),
     });
     await schema.validate(data, { abortEarly: false });
-    const cliente = await clienteRepository.update(codigo, data);
+    const cliente = await clienteRepository.update(id, data);
     return response.status(201).json(cliente);
   },
 };
