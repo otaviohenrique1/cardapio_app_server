de5c9d233805683417ff7a671e5cacbadc0b9bc6
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-08 18:47:08 -0300

    removendo usuario_codigo da url

--- src/views/ImagemView.ts
+++ src/views/ImagemView.ts
@@ -2,12 +2,13 @@ import { Imagem } from "../entity/Imagem";
 
 export default {
   render(imagem: Imagem) {
-    const usuario_codigo = imagem.refeicao.usuario.codigo;
-    const imagem_path = imagem.path;
+    // const usuario_codigo = imagem.refeicao.usuario.codigo;
+    // const imagem_path = imagem.path;
     return {
       id: imagem.id,
-      url: `http://192.168.0.12:3333/uploads/fotos/${usuario_codigo}/${imagem_path}`,
+      url: `http://192.168.0.12:3333/uploads/fotos/${imagem.path}`,
     };
+    // url: `http://192.168.0.12:3333/uploads/fotos/${usuario_codigo}/${imagem_path}`,
     // url: `http://localhost:3333/uploads/${image.path}`,
   },
 
