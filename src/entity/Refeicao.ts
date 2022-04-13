6cc6290278c7b9d58a06954229ac7d8ec2ca09e0
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 20:29:18 -0300

    removendo codigo comentado

--- src/entity/Refeicao.ts
+++ src/entity/Refeicao.ts
@@ -40,39 +40,3 @@ export default class Refeicao extends BaseEntity {
   @ManyToOne(() => Usuario, (usuario) => usuario.refeicoes)
   usuario: Usuario;
 }
-
-/*
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
-import Usuario from "./Usuario";
-
-@Entity('refeicao')
-export default class Refeicao {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  nome: string;
-
-  @Column()
-  preco: number;
-
-  @Column()
-  ingredientes: string;
-
-  @Column()
-  ativo: boolean;
-
-  @Column()
-  codigo: string;
-  
-  @Column()
-  data_cadastro: Date;
-
-  @Column()
-  data_modificacao_cadastro: Date;
-
-  @ManyToOne(() => Usuario, (usuario) => usuario.refeicoes)
-  @JoinColumn({ name: 'refeicoes' })
-  id_usuario: number;
-}
-*/
--- src/entity/Usuario.ts
+++ src/entity/Usuario.ts
@@ -28,37 +28,3 @@ export default class Usuario extends BaseEntity {
   @OneToMany(() => Refeicao, (refeicao) => refeicao.usuario)
   refeicoes: Refeicao[];
 }
-
-/*
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-// import { JoinColumn } from "typeorm";
-import Refeicao from "./Refeicao";
-
-@Entity('usuario')
-export default class Usuario {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  nome: string;
-
-  @Column()
-  email: string;
-
-  @Column()
-  senha: string;
-
-  @Column()
-  codigo: string;
-
-  @Column()
-  data_cadastro: Date;
-
-  @Column()
-  data_modificacao_cadastro: Date;
-  
-  @OneToMany(() => Refeicao, (refeicao) => refeicao.id_usuario)
-  // @JoinColumn({ name: 'id_usuario' })
-  refeicoes: Refeicao[];
-}
-*/
