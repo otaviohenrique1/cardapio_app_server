fd16719a643fc4dd8c561bb8648dd7f3f800c2e7
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-21 14:48:38 -0300

    alterando variavel url do metodo render

--- src/views/ImagemView.ts
+++ src/views/ImagemView.ts
@@ -4,15 +4,13 @@ import { Imagem } from "../entity/Imagem";
 
 export default {
   render(imagem: Imagem) {
-    const {
-      id, path
-    } = imagem;
+    const { id, path } = imagem;
 
-    const url = `http://192.168.0.12:${PORT}/uploads/fotos/${path}`;
+    /* Mudar quando for para producao */
+    // const url = `http://192.168.0.12:${PORT}/uploads/fotos/${path}`;
+    const url = `http://localhost:${PORT}/uploads/fotos/${path}`;
 
-    return {
-      id, url
-    };
+    return { id, url };
     // url: `http://192.168.0.12:3333/uploads/fotos/${imagem_path}`,
     // url: `http://localhost:3333/uploads/${image.path}`,
   },
