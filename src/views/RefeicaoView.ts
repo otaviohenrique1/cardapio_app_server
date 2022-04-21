3dbf819637c9df2ecc71a4ad4a0ee85130fd5edc
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-21 14:51:28 -0300

    alterando render

--- src/views/RefeicaoView.ts
+++ src/views/RefeicaoView.ts
@@ -5,16 +5,16 @@ import IngredienteView from "./IngredienteView";
 export default {
   render(refeicao: Refeicao) {
     const {
-      id, nome, preco, ingredientes, descricao, ativo, codigo,
-      data_cadastro, data_modificacao_cadastro, usuario, imagens
+      id, nome, preco, ingredientes, imagens, descricao, ativo, codigo,
+      data_cadastro, data_modificacao_cadastro, usuario
     } = refeicao;
     
     return {
       id, nome, preco,
       ingredientes: IngredienteView.renderMany(ingredientes),
+      imagens: ImagemView.renderMany(imagens),
       descricao, ativo, codigo, data_cadastro,
       data_modificacao_cadastro, usuario,
-      imagens: ImagemView.renderMany(imagens),
     };
   },
   renderMany(refeicoes: Refeicao[]) {
