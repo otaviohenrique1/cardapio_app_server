f4c536b5a18a10fe4af3ce59974caa915e121338
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-26 09:42:39 -0300

    alterando render

--- src/views/ImagemView.ts
+++ src/views/ImagemView.ts
@@ -1,6 +1,5 @@
-import { PORT } from "../utils/constantes";
+import { HOST, PORT, URL_CAMINHO } from "../utils/constantes";
 import { Imagem } from "../entity/Imagem";
-
 
 export default {
   render(imagem: Imagem) {
@@ -8,9 +7,11 @@ export default {
 
     /* Mudar quando for para producao */
     // const url = `http://192.168.0.12:${PORT}/uploads/fotos/${path}`;
-    const url = `http://localhost:${PORT}/uploads/fotos/${path}`;
-
-    return { id, url };
+    // const url = `http://localhost:${PORT}/uploads/fotos/${path}`;
+    
+    return { 
+      id, url: URL_CAMINHO, nome: path
+    };
     // url: `http://192.168.0.12:3333/uploads/fotos/${imagem_path}`,
     // url: `http://localhost:3333/uploads/${image.path}`,
   },
