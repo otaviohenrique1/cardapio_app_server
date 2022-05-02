57245df3ab177e7ea2401e8d7740bbbb634cf6e4
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-05-02 16:28:08 -0300

    adicionando comentarios e reformulando arquivos

--- src/entity/PedidoRefeicao.ts
+++ src/entity/PedidoRefeicao.ts
@@ -1,16 +1,23 @@
-import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Pedido } from "./Pedido";
+import { PedidoIngredienteRemovido } from "./PedidoIngredienteRemovido";
+import { PedidoOpcionalAdicionado } from "./PedidoOpcionalAdicionado";
 import Refeicao from "./Refeicao";
 
+ /* Ver se vai renomear de 'PedidoRefeicao' para 'PedidoItem' ou 'PedidoProduto' */
 @Entity('pedido_refeicao')
 export class PedidoRefeicao extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  /* Arrumar */
+  /* Ver se vai mudar de 'OneToOne' para 'OneToMany'*/
+  /* Ver se vai renomear de 'refeicaoId' para produtoId */
   @OneToOne(() => Refeicao, (refeicao) => refeicao.pedido_refeicao)
   @JoinColumn({ name: 'refeicaoId' })
-  refeicao: Pedido;
+  refeicao: Refeicao;
 
+  /* Ver se vai renomear de 'refeicaoId' para produtoId */
   @Column({ type: 'integer', unsigned: true })
   refeicaoId: number;
 
@@ -24,4 +31,18 @@ export class PedidoRefeicao extends BaseEntity {
 
   @Column({ type: 'integer', unsigned: true })
   pedidoId: number;
+
+  /* 1 refeicao do pedido pode ter nenhum ou 1 ou mais opcionais */
+  /* Ver se vai ser opcional */
+  @OneToMany(() => PedidoOpcionalAdicionado, opcional => opcional.pedido_refeicao, {
+    cascade: ['insert']
+  })
+  lista_opcionais: PedidoOpcionalAdicionado[];
+
+  /* Nenhum ou 1 ou mais ingredientes podem ser removidos de 1 refeicao */
+  /* Ver se vai ser opcional */
+  @OneToMany(() => PedidoIngredienteRemovido, ingrediente_removido => ingrediente_removido.pedido_refeicao, {
+    cascade: ['insert']
+  })
+  lista_ingredientes_removidos: PedidoIngredienteRemovido[];
 }
--- src/entity/Refeicao.ts
+++ src/entity/Refeicao.ts
@@ -4,6 +4,7 @@ import { Ingrediente } from "./Ingrediente";
 import { PedidoRefeicao } from "./PedidoRefeicao";
 import Usuario from "./Usuario";
 
+/* Ver se vai mudar o nome de 'Refeicao' para 'Produto' */
 @Entity('refeicao')
 export default class Refeicao extends BaseEntity {
   @PrimaryGeneratedColumn()
@@ -28,8 +29,19 @@ export default class Refeicao extends BaseEntity {
   @Column()
   descricao: string;
 
+  /* ativo => status_produto ou situacao_produto */
   @Column()
-  ativo: boolean;
+  ativo: boolean; // 'ativo' => true, 'inativo' => false
+  // ativo: string; // ('ativo', 'inativo', 'rejeitado', 'testes'), caso seja do tipo string
+
+  @Column()
+  quantidade: number;
+
+  @Column()
+  unidade_quantidade: string; // unidade, ml, l, mg, g, kg
+
+  @Column()
+  tipo: string; // ('comida' e 'bebida')
 
   /* 1 refeicao com 1 ou mais imagens */
   @OneToMany(() => Imagem, imagem => imagem.refeicao, {
@@ -46,11 +58,22 @@ export default class Refeicao extends BaseEntity {
   usuarioId: number;
 
   /* 1 refeicao com 1 ou mais ingredientes */
+  /* Ver se vai ser opcional */
   @OneToMany(() => Ingrediente, ingrediente => ingrediente.refeicao, {
     cascade: ['insert', 'update', 'remove']
   })
   ingredientes: Ingrediente[];
 
+  /* 1 refeicao do pedido pode ter nenhum ou 1 ou mais opcionais */
+  /* Ver se vai ser opcional */
+  /* Ver se vai ser cadastrado no cadastro da refeicao */
+  // @OneToMany(() => PedidoOpcionalAdicionado, opcional => opcional.pedido_refeicao, {
+  //   cascade: ['insert']
+  // })
+  // lista_opcionais: PedidoOpcionalAdicionado[];
+
+  /* Arrumar */
+  /* Ver se vai renomear de 'PedidoRefeicao' para 'PedidoItem' ou 'PedidoProduto' */
   @OneToOne(() => PedidoRefeicao, (pedido_refeicao) => pedido_refeicao.refeicao)
   pedido_refeicao: PedidoRefeicao;
 }
